2927d4e9a1151a715ee1798042d7d210ea7b96b6
Brimcage <151949436+banijames@users.noreply.github.com>
2026-07-05 22:18:58 +0530

    Time: 0 ms (100%), Space: 19.3 MB (58.01%) - LeetHub

---
+++ 3046-split-the-array/3046-split-the-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPossibleToSplit(self, nums: List[int]) -> bool:
+        freq={}
+        for num in nums:
+            freq[num]=freq.get(num,0)+1
+
+            if freq[num]>2:
+                return False
+        
+        return True
