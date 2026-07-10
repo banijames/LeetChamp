5fc3f162b9f8cab8d5ecbe30e46ae363203888a0
Brimcage <151949436+banijames@users.noreply.github.com>
2026-07-10 11:36:36 +0530

    Time: 0 ms (100%), Space: 20.4 MB (61.28%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        k=0
+        for i in nums:
+            if i!=0:
+                nums[k]=i
+                k+=1
+        for i in range(k,len(nums)):
+            nums[i]=0
+
+        
+        
