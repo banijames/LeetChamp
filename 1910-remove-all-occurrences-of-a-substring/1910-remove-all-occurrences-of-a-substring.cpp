4a7f6d2d7e5fd9f33074929598ad5ecfe0faa5da
Brimcage <151949436+banijames@users.noreply.github.com>
2026-08-29 16:55:27 +0530

    Time: 4 ms (15.12%), Space: 9.2 MB (93%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        string result = "";
-        int partLen = part.length();
-        for(char ch : s){
-            result.push_back(ch);//add character to result
-            if(result.length()>=partLen && 
-               result.substr(result.length()-partLen)==part){
-               result.erase(result.length() - partLen);
-                
-            }
+        while(s.length()!=0 && s.find(part)<s.length()){
+            s.erase(s.find(part),part.length());
         }
-        return result;
+        return s;
     }
 };
