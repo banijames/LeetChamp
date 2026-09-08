f5d98fcc1942347952f70e6352b385bf3d2a39f6
Brimcage <151949436+banijames@users.noreply.github.com>
2026-09-08 22:10:23 +0530

    Time: 47 ms (62.66%), Space: 29.2 MB (28.5%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-       sort(nums.begin(), nums.end()); // Array MUST be sorted
-    vector<vector<int>> result;
+    vector<vector<int>> threeSum(vector<int>& nums){
+        sort(nums.begin(),nums.end());
+       //running through the 1st 3 in order to avoid duplicate
+        vector<vector<int>> result;
+        //skip duplicates
+        for(int i=0; i<nums.size()-2; i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
 
-    for (int i = 0; i < (int)nums.size() - 2; i++) {
-        // Skip duplicate values for 'i' to avoid duplicate triplets
-        if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int s = i+1;
+            int e = nums.size()-1;
+            while(s<e){
+                int sum = nums[i]+nums[s]+nums[e];
 
-        int j = i + 1;
-        int k = nums.size() - 1;
-
-        while (j < k) {
-            int sum = nums[i] + nums[j] + nums[k];
-
-            if (sum == 0) {
-                result.push_back({nums[i], nums[j], nums[k]});
+                if(sum==0){
+                    result.push_back({nums[i],nums[s],nums[e]});
                 
-                // Skip duplicates for 'j' and 'k'
-                while (j < k && nums[j] == nums[j + 1]) j++;
-                while (j < k && nums[k] == nums[k - 1]) k--;
-
-                j++;
-                k--;
-            } 
-            else if (sum < 0) {
-                j++; // Sum too small -> move left pointer right
-            } 
-            else {
-                k--; // Sum too large -> move right pointer left
+                    //skip duplicates of s and e
+                    while(s<e && nums[s]==nums[s+1]) s++;
+                
+                    while(s<e && nums[e]==nums[e-1]) e--; 
+                    //move pointers inward
+                    s++;
+                    e--;
+                    
+                }else if(sum<0){
+                    s++;
+                }else{
+                    e--;
+                }
             }
         }
-    }
-    return result;
+        return result;
     }
 };
