6005b00c3b98f782766a474a7ffbbc818fa0147c
Brimcage <151949436+banijames@users.noreply.github.com>
2026-09-09 21:34:54 +0530

    Time: 0 ms (100%), Space: 8.7 MB (7.73%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int sum = 0;
+        if(num == 0) return 0;
+
+        while(num){
+            sum+=(num%10);
+            num/=10;
+        }
+        if(sum<10){
+            return sum;
+        }else{
+            return addDigits(sum);
+        }
+
+    }
+};
