37404397ca8c93154536880d0cfaa68bc349a856
Brimcage <151949436+banijames@users.noreply.github.com>
2026-09-17 20:34:32 +0530

    Time: 0 ms (100%), Space: 31.2 MB (98.83%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int n = nums.size()-1;
-        int right = n;
-        int left = 0;
-
-        while(left<= right){
-            int mid = (left+right)/2;
-            if (target == nums[mid]){
+    int recursivesearch(vector<int>& nums, int target, int s, int e){
+        if(s<=e){
+            int mid = s+(e-s)/2;
+            if(nums[mid]==target){
                 return mid;
             }
-            else if(target < nums[mid]){
-                right = mid-1;
+            if(nums[mid]<target){
+                return recursivesearch(nums,target,mid+1,e);
             }
             else{
-                left = mid+1;
+                return recursivesearch(nums,target,s,mid-1);
             }
         }
         return -1;
+    }
+    int search(vector<int>& nums,int target){
+        int e = nums.size()-1;
+        return recursivesearch(nums,target,0,e);
     }
 };
